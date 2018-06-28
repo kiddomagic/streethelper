cb906de4a443c7d4fccdf57cdcf1c46db869437b
HAINATSE62324\Administrator <hai7f2009@gmail.com>
2018-06-28 22:10:49 +0700

    Edit

--- app/src/main/java/tc/streethelper/streethelper/ContentCharacterActivity.java
+++ app/src/main/java/tc/streethelper/streethelper/ContentCharacterActivity.java
@@ -5,9 +5,17 @@ import android.os.Bundle;
 
 public class ContentCharacterActivity extends AppCompatActivity {
 
+    private TextView mTxtName, mTxtStory, mTxtTarget, mTxtCurrent;
+    private ImageButton imgChr;	
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_character);
+	Intent intent = getIntent();
+	mTxtName = (TextView) findViewById(R.id.textView);
+	mTxtStory = (TextView) findViewById(R.id.editText);
+	mTxtTarget = (TextView) findViewById(R.id.textView3);
+	mTxtCurrent = (TextView) findViewById(R.id.textView4);
     }
 }
